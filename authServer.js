1b1f3c7dd196aa0bc4632c8aa748118fbd24a4f7
walterayiego <walterayiego@gmail.com>
2024-04-29 16:40:21 +0300

    Auth Server

--- authServer.js
+++ authServer.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const jwt = require("jsonwebtoken");
-const port = 3000;
+const port = 4000;
 const users = require("./mockDB");
 
 app.use(express.json());
